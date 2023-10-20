de167dea0cb7c1bf0ccd845d176521792626fae9
berkant0 <dagtekinberkant03@gmail.com>
2023-10-20 16:51:55 +0300

    register table view

--- newsapp/newsapp/Modules/News/View/NewsViewController.swift
+++ newsapp/newsapp/Modules/News/View/NewsViewController.swift
@@ -7,12 +7,31 @@
 
 import UIKit
 
-class NewsViewController: UIViewController {
+final class NewsViewController: UIViewController {
+
+    // MARK: IBOutlets
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        registerTableView()
     }
 
+    func registerTableView() {
+        tableView.register(UINib(nibName: "NewsCell", bundle: nil), forCellReuseIdentifier: "NewsCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
 }
 
+extension NewsViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
